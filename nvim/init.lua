190b8b4cfef2295d1957f707376cbadc0ea86695
dongbin-lee <dongbin-lee@c-fo.com>
2025-04-28 15:08:41 +0900

    refactor(neovim): :recycle: do not read configs on vscode

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,8 @@
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
-require("config.lazy")
-require("config.lsp")
+if vim.g.vscode == 1 then
+else
+  require("config.options")
+  require("config.keymaps")
+  require("config.autocmds")
+  require("config.lazy")
+  require("config.lsp")
+end
