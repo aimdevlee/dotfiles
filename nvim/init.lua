9682c76ed7762992a32ab18d6ddd958eddc61f32
dongbin-lee <dongbin-lee@c-fo.com>
2025-05-01 12:02:02 +0900

    refactor(neovim): remove condition for vscode

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,5 @@
-if vim.g.vscode == 1 then
-else
-  require("config.options")
-  require("config.keymaps")
-  require("config.autocmds")
-  require("config.lazy")
-  require("config.lsp")
-end
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+require("config.lazy")
+require("config.lsp")
