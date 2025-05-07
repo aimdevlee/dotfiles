c838a60408c342cb488f6442b21ec35aead7c39a
dongbin-lee <dongbin-lee@c-fo.com>
2025-05-07 15:05:53 +0900

    refactor(neovim): remove nvim-lint

--- nvim/lua/plugins/editing-support.lua
+++ nvim/lua/plugins/editing-support.lua
@@ -127,21 +127,4 @@ return {
       vim.api.nvim_set_hl(0, "BlinkCmpDocSeparator", { link = "Special" }) -- as same as signature in noice
     end,
   },
-  {
-    "mfussenegger/nvim-lint",
-    event = "BufReadPost",
-    config = function()
-      require("lint").linters_by_ft = {
-        javascript = { "eslint" },
-        typescript = { "eslint" },
-        typescriptreact = { "eslint" },
-      }
-
-      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
-        callback = function()
-          require("lint").try_lint()
-        end,
-      })
-    end,
-  },
 }
