edde3d74603d6d5999f50ef7b03f7e360af4ef56
dongbin-lee <dongbin-lee@c-fo.com>
2025-05-08 09:41:49 +0900

    chore(neovim): stop use resize mode

--- nvim/lua/plugins/utility.lua
+++ nvim/lua/plugins/utility.lua
@@ -87,9 +87,6 @@ return {
     config = function()
       require("smart-splits").setup({ at_edge = "stop" })
       local keymap = vim.keymap
-      -- resize
-      keymap.set("n", "<C-w>/", require("smart-splits").start_resize_mode)
-
       -- move between pane in nvim or tmux or terminal multiplexer
       keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
       keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
