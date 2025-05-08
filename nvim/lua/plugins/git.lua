00686d4c2dc5da4079593086bb6ea481d54a9628
dongbin-lee <dongbin-lee@c-fo.com>
2025-05-08 18:37:43 +0900

    feaet(neovim): update gitsigns config

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -21,6 +21,7 @@ return {
     event = "VeryLazy",
     config = function()
       vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { fg = "#909aa0" })
+      vim.api.nvim_set_hl(0, "FloatBorder", { link = "NoiceCmdlinePopupBorder" })
       require("gitsigns").setup({
         signs = {
           add = { text = "│" },
@@ -36,6 +37,9 @@ return {
           virt_text_pos = "eol", -- eol | overlay | right_align
           delay = 500,
         },
+        preview_config = {
+          border = "rounded",
+        },
         current_line_blame_formatter = "  <author>,  <author_time:%R> - <summary>",
         on_attach = function(bufnr)
           local function map(mode, l, r, opts)
@@ -44,12 +48,14 @@ return {
             vim.keymap.set(mode, l, r, opts)
           end
 
-          map("n", "ghh", "<Cmd>Gitsigns preview_hunk<CR>", { desc = "Preview Hunk" })
-          map("n", "ghn", "<Cmd>Gitsigns next_hunk<CR>", { desc = "Next Hunk" })
-          map("n", "ghp", "<Cmd>Gitsigns prev_hunk<CR>", { desc = "Prev Hunk" })
-          map("n", "ghb", "<Cmd>Gitsigns blame_line<CR>", { desc = "Blame Line" })
-          map("n", "ght", "<Cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Current Line Blame" })
-          map("n", "ghr", "<Cmd>Gitsigns reset_hunk<CR>", { desc = "Reset Hunk" })
+          -- hunk
+          map("n", "<leader>ghh", "<Cmd>Gitsigns preview_hunk<CR>", { desc = "Preview Hunk" })
+          map("n", "<leader>ghr", "<Cmd>Gitsigns reset_hunk<CR>", { desc = "Reset Hunk" })
+          map("n", "<leader>ghn", "<Cmd>Gitsigns next_hunk<CR>", { desc = "Next Hunk" })
+          map("n", "<leader>ghp", "<Cmd>Gitsigns prev_hunk<CR>", { desc = "Prev Hunk" })
+          -- blame
+          map("n", "<leader>gb", "<Cmd>Gitsigns blame_line<CR>", { desc = "Blame Line" })
+          map("n", "<leader>gt", "<Cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Current Line Blame" })
         end,
       })
     end,
