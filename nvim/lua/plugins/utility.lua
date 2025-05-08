56ba2c27e3c243c9de261f6ce9825ae425752ebe
dongbin-lee <dongbin-lee@c-fo.com>
2025-05-08 18:37:58 +0900

    feat(neovim): add harpoon

--- nvim/lua/plugins/utility.lua
+++ nvim/lua/plugins/utility.lua
@@ -1,6 +1,25 @@
 return {
   { "nvim-lua/plenary.nvim", lazy = true },
   {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    config = function()
+      local harpoon = require("harpoon")
+      -- stylua: ignore start
+      vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Add" })
+      vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "List" })
+      vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = 'Go to first' })
+      vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = 'Go to second' })
+      vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = 'Go to third' })
+      vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = 'Go to fourth' })
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+      -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+      -- stylua: ignore end
+    end,
+  },
+  {
     "stevearc/oil.nvim",
     lazy = false,
     config = function()
