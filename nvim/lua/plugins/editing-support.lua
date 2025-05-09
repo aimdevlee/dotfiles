c14825d455b77de0a412e4eb63d927a2b0634c3f
dongbin-lee <dongbin-lee@c-fo.com>
2025-05-09 10:25:40 +0900

    feat(neovim): add leap remote keybinding

--- nvim/lua/plugins/editing-support.lua
+++ nvim/lua/plugins/editing-support.lua
@@ -7,6 +7,9 @@ return {
       leap.set_default_mappings()
       leap.opts.case_sensitive = true
       vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+      vim.keymap.set({ "n", "x", "o" }, "gs", function()
+        require("leap.remote").action()
+      end)
     end,
   },
   {
