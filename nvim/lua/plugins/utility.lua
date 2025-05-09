b5c3f54e6f143c7f921bc95f27fa85ce2293b512
dongbin-lee <dongbin-lee@c-fo.com>
2025-05-09 14:11:27 +0900

    feat(neovim): update keybinding for harpoon

--- nvim/lua/plugins/utility.lua
+++ nvim/lua/plugins/utility.lua
@@ -9,8 +9,8 @@ return {
       vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Add" })
       vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "List" })
       -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
-      vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+      vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end, { desc = "Prev" })
+      vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end, { desc = "Next" })
       -- stylua: ignore end
     end,
   },
