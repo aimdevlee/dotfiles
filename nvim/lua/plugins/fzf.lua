6d3a462231713bd57f9ef2c513b1c539d60a3c59
dongbin-lee <dongbin-lee@c-fo.com>
2025-05-13 14:06:03 +0900

    feat(neovim): update fzf-lua config

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -3,8 +3,10 @@ return {
     "ibhagwan/fzf-lua",
     lazy = false,
     config = function()
-      require("fzf-lua").setup({
-        files = { previewer = false },
+      -- ref. https://github.com/ibhagwan/fzf-lua/blob/b45881a2043d96506ba628f3bc65a4594b179c4e/doc/fzf-lua.txt#L739
+      local fzf = require("fzf-lua")
+      fzf.setup({
+        files = { previewer = true, formatter = "path.filename_first" },
         winopts = {
           row = 0.5,
           height = 0.7,
@@ -13,6 +15,7 @@ return {
             layout = "vertical",
           },
         },
+        fzf_opts = {},
         keymap = {
           builtin = {
             ["<M-Esc>"] = "hide",
@@ -32,31 +35,31 @@ return {
             ["<M-p>"] = "preview-up",
           },
         },
+        buffers = {},
       })
-      local FzfLua = require("fzf-lua")
       local keymap = vim.keymap
 
       -- stylua: ignore start
       -- ref. https://github.com/ibhagwan/fzf-lua?tab=readme-ov-file#commands
-      keymap.set("n", "<leader><leader>", function() FzfLua.resume() end, { desc = "Resume" })
-      keymap.set("n", "<leader>ff", function() FzfLua.files() end, { desc = "Find Files" })
-      keymap.set("n", "<leader>fb", function() FzfLua.buffers() end, { desc = "Find Buffers" })
-      keymap.set("n", "<leader>fc", function() FzfLua.files({ cwd = vim.fn.stdpath("config")}) end, { desc = "Find Config Files" })
-      keymap.set("n", "<leader>fr", function() FzfLua.oldfiles() end, { desc = "Recent" })
-      keymap.set("n", "<leader>fg", function() FzfLua.git_files() end, { desc = "Find Git Files" })
-      keymap.set("n", "<leader>ft", function() FzfLua.treesitter() end, { desc = "Treesitter" })
-      keymap.set("n", "<leader>/", function() FzfLua.lgrep_curbuf() end, { desc = "Live Grep(buffer)" })
-      keymap.set("n", "<leader>?", function() FzfLua.live_grep_glob() end, { desc = "Live Grep" })
+      keymap.set("n", "<leader><leader>", function() fzf.resume() end, { desc = "Resume" })
+      keymap.set("n", "<leader>ff", function() fzf.files() end, { desc = "Find Files" })
+      keymap.set("n", "<leader>fb", function() fzf.buffers() end, { desc = "Find Buffers" })
+      keymap.set("n", "<leader>fc", function() fzf.files({ cwd = vim.fn.stdpath("config")}) end, { desc = "Find Config Files" })
+      keymap.set("n", "<leader>fr", function() fzf.oldfiles() end, { desc = "Recent" })
+      keymap.set("n", "<leader>fg", function() fzf.git_files() end, { desc = "Find Git Files" })
+      keymap.set("n", "<leader>ft", function() fzf.treesitter() end, { desc = "Treesitter" })
+      keymap.set("n", "<leader>/", function() fzf.lgrep_curbuf() end, { desc = "Live Grep(buffer)" })
+      keymap.set("n", "<leader>?", function() fzf.live_grep_glob() end, { desc = "Live Grep" })
 
       -- lsp
-      keymap.set("n", "<leader>la", function() FzfLua.lsp_code_actions() end, { desc = "Lsp Code Actions" })
-      keymap.set("n", "<leader>ld", function() FzfLua.lsp_definitions() end, { desc = "Lsp Definitions" })
-      keymap.set("n", "<leader>lD", function() FzfLua.lsp_declarations() end, { desc = "Lsp Declarations" })
-      keymap.set("n", "<leader>lI", function() FzfLua.lsp_implementations() end, { desc = "Lsp Implementations" })
-      keymap.set("n", "<leader>lr", function() FzfLua.lsp_references() end, { desc = "Lsp References" })
-      keymap.set("n", "<leader>ls", function() FzfLua.lsp_document_symbols() end, { desc = "Lsp Document Symbols" })
-      keymap.set("n", "<leader>lS", function() FzfLua.lsp_workspace_symbols() end, { desc = "Lsp Workspace Symbols" })
-      keymap.set("n", "<leader>lt", function() FzfLua.lsp_typedefs() end, { desc = "Lsp Type Definitions" })
+      keymap.set("n", "<leader>la", function() fzf.lsp_code_actions() end, { desc = "Lsp Code Actions" })
+      keymap.set("n", "<leader>ld", function() fzf.lsp_definitions() end, { desc = "Lsp Definitions" })
+      keymap.set("n", "<leader>lD", function() fzf.lsp_declarations() end, { desc = "Lsp Declarations" })
+      keymap.set("n", "<leader>lI", function() fzf.lsp_implementations() end, { desc = "Lsp Implementations" })
+      keymap.set("n", "<leader>lr", function() fzf.lsp_references() end, { desc = "Lsp References" })
+      keymap.set("n", "<leader>ls", function() fzf.lsp_document_symbols() end, { desc = "Lsp Document Symbols" })
+      keymap.set("n", "<leader>lS", function() fzf.lsp_workspace_symbols() end, { desc = "Lsp Workspace Symbols" })
+      keymap.set("n", "<leader>lt", function() fzf.lsp_typedefs() end, { desc = "Lsp Type Definitions" })
       -- stylua: ignore end
     end,
   },
