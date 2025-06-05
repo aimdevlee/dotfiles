cfa642668d04c651f26de53a4f525f9b0acbb5bd
dongbin-lee <dongbin-lee@c-fo.com>
2025-06-05 19:07:38 +0900

    feat(neovim): update markdown plugin option

--- nvim/lua/plugins/editing-support.lua
+++ nvim/lua/plugins/editing-support.lua
@@ -139,6 +139,14 @@ return {
     config = function()
       require("render-markdown").setup({
         completions = { blink = { enabled = true } },
+        win_options = {
+          conceallevel = { rendered = 2 },
+        },
+        heading = {
+          enabled = false,
+          position = "inline",
+          atx = false,
+        },
       })
     end,
   },
