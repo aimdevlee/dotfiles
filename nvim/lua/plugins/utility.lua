cf7bd4b88417eb257d961bd4261e76d5e7848856
dongbin-lee <dongbin-lee@c-fo.com>
2025-06-05 19:07:23 +0900

    feat(neovim): add obsidian.nvim

--- nvim/lua/plugins/utility.lua
+++ nvim/lua/plugins/utility.lua
@@ -108,4 +108,33 @@ return {
       keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
     end,
   },
+  {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    keys = {
+      { "<leader>os", "<cmd>Obsidian search<cr>", desc = "Search Note" },
+      { "<leader>of", "<cmd>Obsidian quick_switch<cr>", desc = "Find Note" },
+      { "<leader>on", "<cmd>Obsidian new<cr>", desc = "Create New Note" },
+      { "<leader>ot", "<cmd>Obsidian today<cr>", desc = "Create or Open Today Note" },
+    },
+    ft = "markdown",
+    config = function()
+      require("obsidian").setup({
+        workspaces = {
+          {
+            name = "work",
+            path = "~/vaults/work",
+          },
+        },
+        completion = {
+          nvim_cmp = false,
+          blink = true,
+        },
+        picker = {
+          name = "snacks.pick",
+        },
+      })
+    end,
+  },
 }
