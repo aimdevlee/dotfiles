19fa5dd2ce67a72c71c41c11c1c43cbede077765
aimdevlee <aimdevlee@gmail.com>
2025-11-07 22:44:02 +0900

    fix(nvim): refactor restart keymap

--- .config/nvim/lua/plugins/persistence.lua
+++ .config/nvim/lua/plugins/persistence.lua
@@ -1,8 +1,15 @@
 return {
   'folke/persistence.nvim',
   event = 'BufReadPre',
+  keys = {
+    {
+      '<leader>R',
+      '<cmd>wall | restart lua require("persistence").load({last=true})<CR>',
+      desc = 'Restart Neovim',
+      mode = { 'n', 'v', 'x' },
+    },
+  },
   config = function()
     require('persistence').setup()
   end,
 }
-
