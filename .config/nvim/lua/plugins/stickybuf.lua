04bc7f2931c45f424c286a4505103ad448751855
aimdevlee <aimdevlee@gmail.com>
2025-11-10 23:16:05 +0900

    fix(nvim): exclude dap-view ft from stickybuf.pin

--- .config/nvim/lua/plugins/stickybuf.lua
+++ .config/nvim/lua/plugins/stickybuf.lua
@@ -1,6 +1,16 @@
 return {
   'stevearc/stickybuf.nvim',
   config = function()
-    require('stickybuf').setup()
+    require('stickybuf').setup({
+      get_auto_pin = function(bufnr)
+        local buf = vim.api.nvim_get_option_value('filetype', { buf = bufnr })
+
+        if buf == 'dap-view' then
+          return nil
+        end
+
+        return require('stickybuf').should_auto_pin(bufnr)
+      end,
+    })
   end,
 }
