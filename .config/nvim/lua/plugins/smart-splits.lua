706b98ff812766647a0a4ad6328cf3865a11695b
aimdevlee <aimdevlee@gmail.com>
2026-01-30 22:35:43 +0900

    fix(nvim/tmux): improve navigation with arrow-keys

--- .config/nvim/lua/plugins/smart-splits.lua
+++ .config/nvim/lua/plugins/smart-splits.lua
@@ -1,13 +1,20 @@
 return {
   'mrjones2014/smart-splits.nvim',
   config = function()
-    require('smart-splits').setup()
+    require('smart-splits').setup({})
 
     local keymap = vim.keymap
     -- move between pane in nvim or tmux or terminal multiplexer
-    keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up, { desc = 'Move to Upper Window (Tmux aware)' })
-    keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down, { desc = 'Move to Lower Window (Tmux aware)' })
-    keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left, { desc = 'Move to Left Window (Tmux aware)' })
-    keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right, { desc = 'Move to Right Window (Tmux aware)' })
+    keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+    keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+    keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+    keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+    keymap.set('n', '<C-Up>', require('smart-splits').move_cursor_up)
+
+    -- navigate with arrow keys for colemak users
+    keymap.set('n', '<C-Left>', require('smart-splits').move_cursor_left)
+    keymap.set('n', '<C-Down>', require('smart-splits').move_cursor_down)
+    keymap.set('n', '<C-Up>', require('smart-splits').move_cursor_up)
+    keymap.set('n', '<C-Right>', require('smart-splits').move_cursor_right)
   end,
 }
